8a8e489b648f04b2ea11297620b4daf96585ccb0
VertigoCool99 <83370667+VertigoCool99@users.noreply.github.com>
2023-03-28 13:17:27 +0100

    Update Uii.lua

--- Uii.lua
+++ Uii.lua
@@ -2599,7 +2599,7 @@ end;
 
 function Library:SetWatermark(Text)
     local X, Y = Library:GetTextBounds(Text, Library.Font, 14);
-    Library.Watermark.Size = UDim2.new(0, X + 15, 0, (Y * 1.5) + 3);
+    Library.Watermark.Size = UDim2.new(0, Library.WatermarkText.TextBounds.X + 15, 0, (Y * 1.5) + 3);
     Library:SetWatermarkVisibility(true)
 
     game:GetService("RunService").RenderStepped:Connect(function(deltaTime)
