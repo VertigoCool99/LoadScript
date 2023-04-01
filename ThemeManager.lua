51c0dce52cc2855e9aba3aad848b40c67cc543af
VertigoCool99 <83370667+VertigoCool99@users.noreply.github.com>
2023-04-01 21:39:51 +0100

    Update ThemeManager.lua

--- ThemeManager.lua
+++ ThemeManager.lua
@@ -48,7 +48,7 @@ local ThemeManager = {} do
 	end
 
 	function ThemeManager:LoadDefault()		
-		local theme = 'Default'
+		local theme = 'Project Vertigo'
 		local content = isfile(self.Folder .. '/themes/default.txt') and readfile(self.Folder .. '/themes/default.txt')
 
 		local isDefault = true
