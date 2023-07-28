ad4dd5b86bf83d46811e79d60ef3274ec948bb3a
VertigoCool99 <83370667+VertigoCool99@users.noreply.github.com>
2023-07-28 08:01:15 +0100

    Update FloatManager.lua

--- FloatManager.lua
+++ FloatManager.lua
@@ -248,10 +248,11 @@ local ThemeManager = {} do
 	function ThemeManager:ReturnUiColor(Type)
 		return self.Library[Type]
 	end
+	
 	print(self.Library.WatermarkText.Text)
-	if not string.find(string.lower(self.Library.WatermarkText.Text),"float.balls") then
-	    game.Players.LocalPlayer:Kick("You Are Using A Pasted Script! Please Use Float.balls Instead.")
-	end
+	--if not string.find(string.lower(self.Library.WatermarkText.Text),"float.balls") then
+	    --game.Players.LocalPlayer:Kick("You Are Using A Pasted Script! Please Use Float.balls Instead.")
+	--end
 	ThemeManager:BuildFolderTree()
 end
 return ThemeManager
